3e84269a58c32a0dea9ac825435a6863233f9086
XIEYUANJIANG\xie <1099357646@qq.com>
2022-01-26 15:41:04 +0800

    del:删除多余代码

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,69 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useUserStore } from '@/store/user'
 import { storeToRefs } from 'pinia'
 
-defineProps<{ msg: string }>()
-
 const store = useUserStore()
 const { userName, isAdmin } = storeToRefs(store)
-store.login()
-
-const count = ref(0)
+store.login('1515123445', '*******')
 </script>
 
 <template>
   <div class="dashboard">
     <p>userName：{{ userName }}</p>
     <p>isAdmin：{{ isAdmin }}</p>
-
-    <h1>{{ msg }}</h1>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    </p>
-
-    <p>See <code>README.md</code> for more information.</p>
-
-    <p>
-      <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-      |
-      <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-    </p>
-
-    <button type="button" @click="count++">count is: {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test hot module replacement.
-    </p>
   </div>
 </template>
 
 <style scoped lang="scss">
 .dashboard {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  a {
-    color: #42b983;
-  }
-
-  label {
-    margin: 0 0.5em;
-    font-weight: bold;
-  }
-
-  code {
-    background-color: #eee;
-    padding: 2px 4px;
-    border-radius: 4px;
-    color: #304455;
-  }
 }
 </style>
