8aa3efee8359ce38d2fcdb041ad112a5190baa75
Xyuan <1099357646@qq.com>
2022-02-15 11:01:40 +0800

    update  更新vite打包删除console和debugger配置

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,12 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    terserOptions: {
+      compress:{
+        drop_console: true,
+        drop_debugger: true,
+      }
+    },
+  },
 })
